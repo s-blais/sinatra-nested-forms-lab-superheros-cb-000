bfbe840eea85ce317c79d738e9588c93e50bdf94
Stephen Blais <stephen@s-blais.com>
2019-10-30 20:34:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      binding.pry
+      # binding.pry
       @team = Team.new(params[:team])
       # binding.pry
       params[:team][:members].each do |details|
